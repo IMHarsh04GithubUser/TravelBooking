a5a6d6ad04d8c278d39aa20b9d8d896a48fd5756
dhruvmathur2001 <dhruvmathur2001@gmail.com>
2025-08-07 19:35:01 +0530

    add Hero section

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero:React.FC = () => {
+  return (
+    <div className='hero_container border border-black bg-black text-white'>
+        <div className="flex flex-col gap-6 p-4 my-34">
+            <div className="">
+                <p className='text-center text-7xl'>Discover Your Next Adventure</p>
+            </div>
+            <div className="">
+                <p className='text-center text-2xl'>Book Flights, Hotels and complete Travel Packages at the best prices.<br/> <span>Your journey starts here.</span></p>
+            </div>
+        </div>
+        
+        
+      
+    </div>
+  )
+}
+
+export default Hero
